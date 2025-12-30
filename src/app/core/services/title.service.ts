ef1a6c6125020cf9318b1af091db2e714fd473fc
Sharique Ansari <shariquedotnet@gmail.com>
2025-12-30 19:01:28 +0530

    update environment variable reference in title service

--- src/app/core/services/title.service.ts
+++ src/app/core/services/title.service.ts
@@ -2,16 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Title, TitleViewModel } from '../models/title.model';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TitleService {
-  private readonly controllerUrl = '/api/Title';
   private apiUrl = `${environment.apiEndpoint}/Title`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTitles(page: number = 0, pageSize: number = 10, name: string = ''): Observable<TitleViewModel> {
     return this.http.get<TitleViewModel>(`${this.apiUrl}/titleList/${page}/${pageSize}/${name}`);
